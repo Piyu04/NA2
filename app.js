322fd8a8c7161a984a2bf943b592f7e022d4e19b
LTP-PRIYAD\LTP-PriyaD <priydarshani@pragiti.com>
2018-09-18 17:11:02 +0530

    [committed code after for displaying user databse onto the browser.]

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//view engine
+app.set('views',__dirname+'/views');
+app.set('view engine' ,'jade');
+
+// mongoose connection
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/SampleProductRequestDatabase");
 
 var sampleproductRequestsSchema = new mongoose.Schema({
-    usertId: String,
+    emailId: String,
     productId: String
 });
 
@@ -20,15 +25,18 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/sample-product-request.html");
 });
 
+app.get('/view',function(req,res){
+    User.find({},function(err,docs){
+		if(err) res.json(err);
+		else res.render('list',{users : docs});
+	});
+});
+
 app.post("/sampleProductRequest", (req, res) => {
     var myData = new User(req.body);
-    myData.save()
-    .then(item => {
-    res.send("item saved to database"+item);
-    res.send(myData);
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
+    myData.save(function(err,doc){
+        if(err) res.json(err);
+        else res.redirect('/view')
     });
    });
 
